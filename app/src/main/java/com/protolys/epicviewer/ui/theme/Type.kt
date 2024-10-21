d2667d20891ffbfbef66b3778e5658f61ba477b5
nbalasundara <info.protolys@gmail.com>
2024-10-21 09:34:43 -0400

    Font issue with Material 3 fixed.

--- app/src/main/java/com/protolys/epicviewer/ui/theme/Type.kt
+++ app/src/main/java/com/protolys/epicviewer/ui/theme/Type.kt
@@ -16,76 +16,155 @@ import com.protolys.epicviewer.R
 
 //
 // Fonts from: https://fonts.google.com/specimen/Space+Grotesk
-val spaceGroteskFamily = FontFamily(
-    Font(R.font.spacegrotesk_bold, Bold),
-    Font(R.font.spacegrotesk_light, Light),
-    Font(R.font.spacegrotesk_medium, Medium),
-    Font(R.font.spacegrotesk_regular, Normal),
-    Font(R.font.spacegrotesk_semibold, SemiBold)
-)
-
-val interFamily = FontFamily(
-    Font(R.font.inter_18pt_bold, Bold),
-    Font(R.font.inter_18pt_regular, Normal),
-    Font(R.font.inter_18pt_semibold, SemiBold),
-    Font(R.font.inter_24pt_bold, Bold),
-    Font(R.font.inter_24pt_regular, Normal),
-    Font(R.font.inter_24pt_semibold, SemiBold),
-    Font(R.font.inter_28pt_bold, Bold),
-    Font(R.font.inter_28pt_regular, Normal),
-    Font(R.font.inter_28pt_semibold, SemiBold)
-)
-
+object AppFont {
+    val SpaceGroteskFamily = FontFamily(
+        Font(R.font.spacegrotesk_bold, Bold),
+        Font(R.font.spacegrotesk_light, Light),
+        Font(R.font.spacegrotesk_medium, Medium),
+        Font(R.font.spacegrotesk_regular, Normal),
+        Font(R.font.spacegrotesk_semibold, SemiBold)
+    )
+    val InterFamily = FontFamily(
+        Font(R.font.inter_18pt_bold, Bold),
+        Font(R.font.inter_18pt_regular, Normal),
+        Font(R.font.inter_18pt_semibold, SemiBold),
+        Font(R.font.inter_24pt_bold, Bold),
+        Font(R.font.inter_24pt_regular, Normal),
+        Font(R.font.inter_24pt_semibold, SemiBold),
+        Font(R.font.inter_28pt_bold, Bold),
+        Font(R.font.inter_28pt_regular, Normal),
+        Font(R.font.inter_28pt_semibold, SemiBold)
+    )
+}
 
 // Set of Material typography styles
+private val defaultTypography = Typography()
 val Typography = Typography(
-    titleLarge = TextStyle( // H1
-        fontFamily = spaceGroteskFamily,
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = AppFont.SpaceGroteskFamily),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = AppFont.SpaceGroteskFamily),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = AppFont.SpaceGroteskFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = AppFont.SpaceGroteskFamily),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = AppFont.SpaceGroteskFamily), // TODO: should this be Inter?
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = AppFont.InterFamily),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = AppFont.InterFamily),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = AppFont.InterFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = AppFont.InterFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = AppFont.InterFamily)
+)
+/*
+val Typography = Typography(
+    displayLarge = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 26.sp,
-        letterSpacing = 0.sp
+        //letterSpacing = 0.sp
     ),
-    titleMedium = TextStyle( // H2
-        fontFamily = spaceGroteskFamily,
+    displayMedium = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
         fontWeight = FontWeight.Bold,
-        fontSize = 21.sp,
-        lineHeight = 22.sp,
-        letterSpacing = 0.sp
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
     ),
-    titleSmall = TextStyle( // H3
-        fontFamily = spaceGroteskFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 21.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.sp
+    displaySmall = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle( // H1
+        fontFamily = SpaceGroteskFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        //letterSpacing = 0.sp
     ),
     bodyLarge = TextStyle( // body bold
-        fontFamily = interFamily,
+        fontFamily = InterFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        //letterSpacing = 0.sp
     ),
     bodyMedium = TextStyle( // body semibold
-        fontFamily = interFamily,
+        fontFamily = InterFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        //letterSpacing = 0.sp
     ),
     bodySmall = TextStyle( // body regular
-        fontFamily = interFamily,
+        fontFamily = InterFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        //letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle( // label
-        fontFamily = interFamily,
+    labelLarge = TextStyle( // label
+        fontFamily = InterFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.sp
+        //letterSpacing = 0.sp
     ),
-)
+    labelMedium = TextStyle( // label
+        fontFamily = InterFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        //letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle( // label
+        fontFamily = InterFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        //letterSpacing = 0.sp
+    ),
+)*/
