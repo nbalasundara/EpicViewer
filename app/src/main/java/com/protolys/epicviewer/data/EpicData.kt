7e0c23783b21e3d14196ad11c8c34d88830dd48a
nbalasundara <info.protolys@gmail.com>
2024-10-22 10:54:31 -0400

    ViewModel initialize

--- app/src/main/java/com/protolys/epicviewer/data/EpicData.kt
+++ app/src/main/java/com/protolys/epicviewer/data/EpicData.kt
@@ -5,16 +5,13 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.TimeZone
 
-// TODO move to Api
-val API_KEY = "hza2jh9oUxEfKgMH4bn0jZ3fFREiYb30BsbN85Zb"
-
 //
 // Extension functions on String for various date conversions
 // The feeds do not specify timezone but they are in UTC as
 // NASA always uses UTC.
 // Wouldn't have been an issue except the screens majorly use
-// Day names which the user most likely assumes to be the local timezone
-// day.
+// Day names which the user most likely assumes to be the local
+// timezone day.
 fun String.toDate() : Date? {
     val formatter = SimpleDateFormat("yyyy-MM-dd")
     val utcZone: TimeZone = TimeZone.getTimeZone("UTC")
@@ -34,6 +31,7 @@ fun String.slashString() : String? {
       return dateWithoutTime.replace("", "")
 }
 
+//
 // Keep as many classes as possible immutable as they get loaded from EPIC
 // server so that the lists do not cause excessive compose refreshes.
 // The images that may be downloaded will be downloaded separately and maybe cached
